693b972ea9cb2c9a3d196f5ed93ae06500e2bad6
rlrpara <rlr.para@gmail.com>
2021-09-14 20:52:31 -0300

    ajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './models/todos.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: string = 'Minhas Tarefas';
 
   constructor() {
-    this.todos.push('passear com o cachorro.');
-    this.todos.push('ir ao supermercado.');
-    this.todos.push('cortar o cabelo.');
+    this.todos.push(new Todo(1, '', false));
+    this.todos.push(new Todo(1, 'ir ao supermercado.', false));
+    this.todos.push(new Todo(1, 'cortar o cabelo.', false));
+  }
+
+  alteraTexto() {
+    this.title = 'Teste';
   }
 }
---
+++ src/app/models/todos.model.ts
@@ -0,0 +1,8 @@
+export class Todo {
+
+  constructor(
+    public id: number,
+    public todo: string,
+    public done: boolean
+  ) { }
+}
