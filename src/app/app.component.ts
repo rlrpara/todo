8b1c9849e8b9b5de324c90a1feb5ae6f4f5b9a7a
rlrpara <rlr.para@gmail.com>
2021-09-14 20:38:07 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push('passear com o cachorro.');
+    this.todos.push('ir ao supermercado.');
+    this.todos.push('cortar o cabelo.');
+  }
 }
